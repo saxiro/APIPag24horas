f90fb6acc76838ba0be87d9f18f1c1ab70629190
Fabrício Barbosa <fabriciopbrb@gmail.com>
2018-09-04 09:54:32 -0300

    Mysql não se comunicando

--- APIPag24horas/Model/Context/MySQLContext.cs
+++ APIPag24horas/Model/Context/MySQLContext.cs
@@ -1,12 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace APIPag24horas.Model.Context
 {
-    public class MySQLContext: DbContext
+	public class MySQLContext: DbContext
     {
         public MySQLContext(){}
         public MySQLContext(DbContextOptions<MySQLContext>options) :base(options){}
